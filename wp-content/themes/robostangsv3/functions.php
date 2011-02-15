30ec120874725169331334b2473f8b9f7057d9de
Jay Zawrotny <jayzawrotny@gmail.com>
2011-02-15 16:11:13 -0500

    Organized functions.php, setting up extension system.

--- wp-content/themes/robostangsv3/functions.php
+++ wp-content/themes/robostangsv3/functions.php
@@ -1,50 +1,116 @@
 <?php
+
+/* =Constants                     
+--------------------------------------------- */
+define( 'THEME_DIR', preg_replace( "#^.*/wp-content/#", '/wp-content/', dirname(__FILE__) ) );
+define( 'THEME_LIB_DIR', dirname(__FILE__) . '/lib/' );
+
+
+/* =Variables                     
+--------------------------------------------- */
+$loaded_libraries = array();
+
+/* =Actions
+--------------------------------------------- */
 add_action( 'wp_footer', 'grid_overlay' );
 add_action( 'wp_head', 'grid_js' );
-function grid_overlay()
-{
-	echo '<div id="over-grid"></div>
-';	
-}
+add_action( 'init', 'theme_init' );
 
-function grid_js()
+
+/* func theme_init  *//*{{{*/
+function theme_init()
 {
-	echo '<script type="text/javascript">
-	document[\'onkeydown\'] = eventHandler;
-		
-	function eventHandler (e) {
-		var event = e || window.event;
-		if( ( event.altKey && event.keyCode === 186 ) || ( event.altKey && event.keyCode === 59 ) )
-		{
-			if( document.getElementById(\'over-grid\').style.display != \'block\' ) {
-				document.getElementById(\'over-grid\').style.display = \'block\';
-			}else{
-				document.getElementById(\'over-grid\').style.display = \'none\';
-			}
-		}
+	add_theme_support( 'post-thumbnails' );
+
+	if( is_admin() )
+	{
+		get_files( THEME_LIB_DIR . 'meta_boxes' );
 	}
-</script>
-	';
-}
+
+	/* Register Nav Menus  *//*{{{*/
+	register_nav_menus( array(
+			'primary' => __( 'Primary Navigation', 'twentyten' ),
+		) );      
+	register_nav_menus( array(
+			'quick-links' => __( 'Quick Links', 'twentyten' ),
+		) );            
+	/*}}}*/
+	/* enqueue js  *//*{{{*/
+	wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'jTater', THEME_DIR . '/js/rotator.js' );
+	/*}}}*/
+	/* custom post types  *//*{{{*/
+	$rotator_labels = array(
+		'name' => _x( 'Rotator', 'post type general name' ),
+		'menu_item' => 'Rotator',
+	);
+	
+	register_post_type( 'rotator', array(
+		'labels' => $rotator_labels,
+		'public' => true,
+		'publicly_queryable' => false,
+		'exclude_from_search' => true,
+		'show_ui' => true,
+		'rewrite' => false,
+		'capability_type' => 'post',
+		'has_archive' => false,
+		'hierarchical' => false,
+		'menu_position' => 4,
+		'supports' => array( 'title', 'editor', 'author', 'thumbnail', 'custom-fields' ),
+	));/*}}}*/
+}/*}}}*/
+
+/* =Theme Functions
+--------------------------------------------- */
+/* func get_page_links() {{{*/
 function get_page_links()
 {
     global $page_links;
 
 	if( ! $page_links )
 	{
-	$page_links = wp_nav_menu( array( 'container_class' => 'menu-header clearfix', 'echo' => 'false', 'theme_location' => 'primary' ) ); 
+		$page_links = wp_nav_menu( array( 
+			'container_class' => 'menu-header clearfix', 
+			'echo' => 'false', 
+			'theme_location' => 'primary' 
+			) ); 
 	}
 
 	echo $page_links;
 }
-register_nav_menus( array(
-		'primary' => __( 'Primary Navigation', 'twentyten' ),
-	) );      
-register_nav_menus( array(
-		'quick-links' => __( 'Quick Links', 'twentyten' ),
-	) );            
+/*}}}*/
+ /*func get_files(){{{*/
+function get_files($directory, $filter = '/\.php$/')
+{
+	$dir = dir( $directory );
+	$files = array();
+	while( ( $file = $dir->read() ) !== false )
+	{
+		if( ! preg_match( "$filter", $file ) )
+		{
+			continue;
+		}
 
+		$files[] = $directory . '/' . $file;
+	}
 
+	return $files;
+}/*}}}*/
+/*func load_library{{{*/
+function load_library($library_file)
+{
+	global $loaded_libraries;
+	if( ! file_exists( $library_file ) )
+	{
+		return false;
+	}
+    include $library_file;
+	$loaded_libraries[] = $library_file;
+}/*}}}*/
+
+/* =Twentyten Functions
+--------------------------------------------- */
+/* func twentyten_posted_on /*{{{*/
 if ( ! function_exists( 'twentyten_posted_on' ) ) :
 /**
  * Prints HTML with meta information for the current post—date/time and author.
@@ -67,4 +133,35 @@ function twentyten_posted_on() {
 	);
 }
 endif;    
+/*}}}*/
+
+/* =Grid Functions
+--------------------------------------------- */
+/* func grid_overlay  *//*{{{*/
+function grid_overlay()
+{
+	echo '<div id="over-grid"></div>
+';	
+}/*}}}*/
+/* func grid_js */ /*{{{*/
+function grid_js()
+{
+	echo '<script type="text/javascript">
+	document[\'onkeydown\'] = eventHandler;
+		
+	function eventHandler (e) {
+		var event = e || window.event;
+		if( ( event.altKey && event.keyCode === 186 ) || ( event.altKey && event.keyCode === 59 ) )
+		{
+			if( document.getElementById(\'over-grid\').style.display != \'block\' ) {
+				document.getElementById(\'over-grid\').style.display = \'block\';
+			}else{
+				document.getElementById(\'over-grid\').style.display = \'none\';
+			}
+		}
+	}
+</script>
+	';
+}/*}}}*/
+
 ?>
