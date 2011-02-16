eb24c5d43a27b347f76bf45e4a71533e14da37d8
Jay Zawrotny <jayzawrotny@gmail.com>
2011-02-15 20:47:42 -0500

    Finished metabox class, and completed the InputForm class

---
+++ wp-content/themes/robostangsv3/lib/form_builder/abstractform.class.php
@@ -0,0 +1,122 @@
+<?php
+abstract class FormInput
+{
+	protected var $type = '';
+	protected var $name = '';
+	protected var $id = '';
+	protected var $value = '';
+	protected var $options = array();
+    protected var $extra_attributes = '';
+	protected var $class = '';
+
+	function __construct( $type, $name, $id, $value = '', $class = '', $extra_attributes = '' )
+	{
+		$this->type = (string)$type;
+		$this->name = (string)$name;
+		$this->id = (string)$id;
+		$this->value = $value;
+		$this->class = (string)$class;
+		$this->extra_attributes = (string)$extra_attributes;
+	}
+
+	function add_option( $value, $label = false )
+	{
+		$label = ( $label ) ? $label : false;
+		$this->options[] = array( $value, $label );
+	}
+
+	function __toString()
+	{
+		return $this->draw();
+	}
+
+	function getName()
+	{
+		return $this->name;
+	}
+
+	function draw( $echo = false )
+	{
+		$html = '';
+		$tag_open = '';
+		$tag_close = '';
+		$attributes = '';
+		$value = '';
+
+		switch( $this->type )
+		{
+			case 'select':
+				$tag_open = 'select';
+				$value = $this->options_to_str();
+			break;
+			case 'textarea':
+				$tag_open = 'textarea';
+				$tag_close = 'textarea';
+				$value = $this->value;
+			break;
+
+			default:
+				$tag_open = 'input';
+            	$attributes['type'] = $this->type;
+				$attributes['value'] = $this->value;
+			break;
+		}
+
+		$html = "<{$tag_open} ";
+		$html .= $this->attr_to_str( $attributes );
+		$html .= ">";
+
+		if( $value )
+		{
+			$html .= $value;
+		}
+
+		if( $tag_close )
+		{
+			$html .= "</{$tag_close}>";
+		}
+
+		if( $echo )
+		{
+			echo $html;
+		}
+
+		return $html;
+
+
+	}
+	protected function options_to_str()
+	{
+		$html = '';
+
+		foreach( $this->options as $option)
+		{
+			$value = $option[0];
+			$label = $option[1];
+
+			if( !$label )
+			{
+				$label = $value;
+			}
+
+			$html .= "\t<option value=\"{$value}\">{$label}</option>\n";
+		}
+
+		return $html;
+	}
+	protected function attr_to_str($attributes)
+	{
+		$extra_attributes = parse_str( $this->extra_attributes );
+		$attributes = array_merge( $attributes, $extra_attributes );
+        $str = '';
+
+		foreach( (array)$attributes as $name => $value )
+		{
+			$str .= $name . '"' . $value . '" ';
+		}
+
+		$str = preg_replace( "/ $/", '', $str );
+
+		return $str;
+	}
+?>
--- wp-content/themes/robostangsv3/lib/meta_boxes/metabox.class.php
+++ wp-content/themes/robostangsv3/lib/meta_boxes/metabox.class.php
@@ -1,2 +1,88 @@
 <?php
+abstract class MetaBox
+{
+	/* = Abstract Methods = */
+	protected abstract function draw();
+
+	/* = Variables = */
+    protected $settings = array();
+    protected $fields = array();
+	protected $callback = null;
+
+	/* = Constructor = */
+	protected function __construct()
+	{
+		$this->_init();
+	}
+
+	/* = Parent Initiator = 
+	 * Used from the extending class's constructor */
+	protected function _init()
+	{
+		add_action( 'admin_init', array( $this, 'load' ), 1 );
+		add_action( 'save_post', array( $this, 'save_post_data' ) );
+	}
+
+	/* = Load Mechanism =
+	 * The function that gets triggered on the admin init to show the meta box */
+	public function load()
+	{
+		add_meta_box( 
+			$this->settings['id'], 
+			__( $this->settings['title'], $this->settings['name'] . '_textdomain' ),
+			array( $this, draw ),
+			'rotator'
+		);
+	}
+
+	/* = Save Function =
+	 * Proccesses data to be saved */ 
+	protected function save_post_data($post_id)
+	{
+		if ( !wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename(__FILE__) )) {
+			return $post_id;
+		}
+
+		// verify if this is an auto save routine. If it is our form has not been submitted, so we dont want
+		// to do anything
+		if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+		{
+			return $post_id;
+		}		
+
+		$post_data = $this->get_post_data( $_POST );
+
+		if( method_exists( $this, 'save' ) )
+		{
+			$post_data = call_user_func( array( $this, 'save' ), $post_data );
+		}
+
+		foreach( $post_data as $key => $value )
+		{
+			update_post_meta( $post_id, '_' . $key, $value );
+		}
+
+		return true;
+				
+	}
+
+	protected function get_post_data($post_array)
+	{
+		$post_data = array();
+
+		foreach( $fields as $field )
+		{
+			$value = $post_array[ $field->getName() ];
+
+        	if( ! $value  )
+			{
+				continue;	
+			}
+
+            $post_data[] = $value;
+		}
+
+		return $post_data;
+	}
+}
 ?>
