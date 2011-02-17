b0811cf4dffbb67766c4cb8c9820c1f60b529d81
Jay Zawrotny <jayzawrotny@gmail.com>
2011-02-17 02:59:55 -0500

    Slightly documented, but the structure is kind of setup.

--- wp-content/themes/robostangsv3/js/rotator.js
+++ wp-content/themes/robostangsv3/js/rotator.js
@@ -1,15 +1,46 @@
-(function ($) {
-  $.fn.rotator = function (options) {
-     var settings = {
-		'animation' 	: 	'slide-right',
-	};
+/**
+ * A jQuery Rotator Plugin
+ *
+ * Made to play nicely with my WordPress theme framework.
+ * Hopefully.
+ * @author Jay Zawrotny <jayzawrotny@gmail.com>
+ * @version 1.0
+ */
+(function( $ ){
 
-	return this.each(function () {
-    	if( options )
-		{
+  var methods = {
+    init : function( options ) { 
+		var settings = {
+			'direction' : 'up'
+		};
+
+		if( options ) {
 			$.extend( settings, options );
 		}
-    });
+
+
+		return this.each(function() {
+
+		} );
+
+	},
+    show : function( ) { // IS   },
+    hide : function( ) { // GOOD },
+    update : function( content ) { // !!! }
   };
-})(jQuery);
+
+  $.fn.jTater = function( method ) {
+    
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.tooltip' );
+    }    
+  
+  };
+
+})( jQuery );
 
