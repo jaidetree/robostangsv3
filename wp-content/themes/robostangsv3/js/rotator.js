7159c8632b65d92153d792db77f7e4a26227b5da
Jay Zawrotny <jayzawrotny@gmail.com>
2011-02-17 14:47:59 -0500

    The slides are switching but not animated yet.

--- wp-content/themes/robostangsv3/js/rotator.js
+++ wp-content/themes/robostangsv3/js/rotator.js
@@ -17,7 +17,17 @@
 	/**
 	 * The collection of slides
 	 */
-    slides: new Array(),
+	 slides : new Object(),
+
+	/**
+	* Total slides
+	*/
+    total_slides : 0,
+
+	 /*
+	 * The Interval Timer for when to change slides
+	 */
+	 interval: 0,
 
 	/**
 	 * Initiator Function
@@ -39,28 +49,78 @@
 
 
 		return this.each(function() {
-			/**
-			 * Not sure what this does.
-			 */
+
 			var $this = $(this);
             
 			/**
 			 * Start Here
 			 */
 
-
-			methods.get_slides( $this );
-
-			console.log( methods.slides );
+			methods.set_slides( $this );
+            methods.set_container_width($this);
+			methods.start_rotating();
 
 		} );
 
 	},
 
-	get_slides : function(slides_ul) {
+	set_container_width : function(slides_ul) {
+
+		var total_width = 0;
+		var padding = 35;
+
 		$('li', slides_ul).each( function() {
-			methods.slides[ methods.slides.length ] = $(this);
+			   total_width += $(this).width();
 		} );
+
+		total_width += padding * methods.total_slides;
+
+		$(methods.slides).css( 'width', total_width );
+	},
+
+	set_slides : function( slides_ul )
+	{
+		methods.slides = slides_ul;
+		methods.total_slides = $('li', slides_ul).length;
+	},
+
+	start_rotating : function()
+	{
+		methods.interval = setInterval( methods.rotate_slide, 5000 );
+	},
+
+	stop_rotating : function()
+	{
+		clearInterval( methods.interval );
+	},
+
+	rotate_slide : function()
+	{
+		console.log( 'rotated' );
+		methods.next_slide();
+	},
+
+	next_slide : function()
+	{
+		console.log( 'Reposition: slide: ' + methods.current_slide );
+		methods.current_slide++;	
+
+		
+		if( methods.current_slide >= methods.total_slides )
+		{
+			methods.current_slide = 0;
+		}
+
+		methods.reposition_slide( 0 );
+		console.log( 'Switch to slide: ' + methods.current_slide );
+	},
+
+	reposition_slide : function( slide_index )
+	{
+		var slide = $( 'li:eq(' + slide_index + ')', methods.slides );
+		console.log( slide );
+		$(methods.slides).remove( slide );
+		$(methods.slides).append( slide );
 	}
 	
   };
