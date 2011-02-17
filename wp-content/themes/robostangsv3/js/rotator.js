bded9e6371f92a0e7393a71a1ef83d2e2aa890e0
Jay Zawrotny <jayzawrotny@gmail.com>
2011-02-17 12:45:32 -0500

    Added get_slides function

--- wp-content/themes/robostangsv3/js/rotator.js
+++ wp-content/themes/robostangsv3/js/rotator.js
@@ -9,7 +9,27 @@
 (function( $ ){
 
   var methods = {
-    init : function( options ) { 
+	/**
+	 * Current Slide Index
+	 */
+	current_slide : 0,
+
+	/**
+	 * The collection of slides
+	 */
+    slides: new Array(),
+
+	/**
+	 * Initiator Function
+	 *
+	 * Controls our main logic, gathers the slides, finds the next one.
+	 * Sets the event handlers. Whatever needs to be done.
+	 * @param {object} options The options to over-ride the default.
+	 * @constructor
+	 * @return {object} The jQuery object for chainability
+	 * @member JTater
+	*/
+	init : function( options ) { 
 		var settings = {
 			'direction' : 'up'
 		};
@@ -20,16 +40,33 @@
 
 
 		return this.each(function() {
+			/**
+			 * Not sure what this does.
+			 */
+			var $this = $(this);
+            
+			/**
+			 * Start Here
+			 */
+
+
+			methods.get_slides( $this );
+
+			console.log( methods.slides );
 
 		} );
 
 	},
-    show : function( ) { // IS   },
-    hide : function( ) { // GOOD },
-    update : function( content ) { // !!! }
+
+	get_slides : function(slides_ul) {
+		$('li', slides_ul).each( function() {
+			methods.slides[ methods.slides.length ] = $(this);
+		} );
+	}
+	
   };
 
-  $.fn.jTater = function( method ) {
+  $.fn.JTater = function( method ) {
     
     // Method calling logic
     if ( methods[method] ) {
